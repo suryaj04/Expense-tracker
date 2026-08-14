e2e460ebab5ff7ee784d72344542794d9834ab11
Surya <jayasuruas274@gmail.com>
2026-08-14 19:39:56 +0530

    changed transaction navigation

--- src/JS/transactions.js
+++ src/JS/transactions.js
@@ -8,15 +8,17 @@ let aside = document.querySelector('aside')
 menuButtons.addEventListener('click',(e)=>{
     let clickedButton = e.target.dataset.action
     if(clickedButton=='menu-open'){
+        document.body.classList.add('overflow-hidden')
         aside.classList.remove('lg:h-screen')
-        aside.classList.add('h-screen')
+        aside.classList.add('h-dvh')
         navigationList.classList.remove('hidden')
         profileCard.classList.remove('hidden')
         menuOpen.classList.add('hidden')
         menuClose.classList.remove('hidden')
     }
     if(clickedButton == 'menu-close'){
-        aside.classList.remove('h-screen')
+         document.body.classList.remove('overflow-hidden')
+        aside.classList.remove('h-dvh')
         aside.classList.add('lg:h-screen')
         navigationList.classList.add('hidden')
         profileCard.classList.add('hidden')
